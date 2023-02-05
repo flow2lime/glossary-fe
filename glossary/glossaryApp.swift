4914ad4af5997bf95ec1506c969984a916d89f39
hopeforhope <91929499+hopeforHope@users.noreply.github.com>
2023-02-05 20:41:32 +0900

    Initial Commit

---
+++ glossary/glossaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  glossaryApp.swift
+//  glossary
+//
+//  Created by hopekim on 2023/02/05.
+//
+
+import SwiftUI
+
+@main
+struct glossaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
